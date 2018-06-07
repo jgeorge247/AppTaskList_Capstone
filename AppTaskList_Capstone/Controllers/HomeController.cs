027a739b7236217d3e459a34d1f03ffce72a2a7c
Justin George <justingeorge247@gmail.com>
2018-06-07 09:38:28 -0400

    first commit

---
+++ AppTaskList_Capstone/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using AppTaskList_Capstone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace AppTaskList_Capstone.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AppTaskList_Capstone/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AppTaskList_Capstone.MvcApplication" Language="C#" %>
